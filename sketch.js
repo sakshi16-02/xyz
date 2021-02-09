064e2e0b90ea2062ff007de03edc3f784c179d78
sakshi mehta <2018pis5341@mnit.ac.in>
2021-02-09 16:31:21 +0530

    added platform

--- sketch.js
+++ sketch.js
@@ -15,7 +15,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
+    platform =new Ground(105,320,300,150)
     ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
@@ -59,4 +59,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
